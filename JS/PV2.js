4a58f2281d3d6a254ee7fcdc0f6db4dfb3484714
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-19 18:09:49 -0500

    cambiospancarta

--- JS/PV2.js
+++ JS/PV2.js
@@ -11,9 +11,9 @@ export class Extras {
         const geometry_situaciones = new THREE.PlaneGeometry( 0.5, 0.4 );
 
 
-        const material_opcion1 = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
+        const material_opcion1 = new THREE.MeshBasicMaterial( {color: 0x00ff00, side: THREE.DoubleSide} );
         const opcion1 = new THREE.Mesh( geometry_situaciones, material_opcion1 );
-        opcion1.position.set(-0.45,6.7,0)
+        opcion1.position.set(-0.45,6.5,0)
         opcion1.rotation.y=THREE.MathUtils.degToRad(90);
         console.log("verificar");
         
